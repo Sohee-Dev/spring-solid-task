49c1dc5439b31e6f5ab980e366d886b6ce1f3d6c
sohee <son5839@naver.com>
2025-10-13 10:10:55 +0900

    연관관계 설정 및 N+1 처리

--- src/main/java/com/puzzlix/solid_task/domain/issue/IssueRepository.java
+++ src/main/java/com/puzzlix/solid_task/domain/issue/IssueRepository.java
@@ -8,4 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IssueRepository extends JpaRepository<Issue, Long> {
     // JpaRepository를 상속 받으면 기본적인 많은 기능을 바로 제공해 준다.
+    // 메서드 선언해서 한방쿼리 적용
+//    @Query("select i from Issue i join fetch i.reporter")
+//    List<Issue> findAllFetchJoin();
 }
